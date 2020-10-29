b7cd837651a2d4b0b99291987b5ee345902ec814
RoLDT <RoLDT@users.noreply.github.com>
2020-10-28 22:25:51 -0600

    Finished UFO Level 1 and Changed the index for it to make more sense. We only have UFO data from January 2010 and not any other month or year.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -22,4 +22,16 @@ function runEnter() {
     var filteredData = tableData.filter(alien => alien.datetime === inputValue);
 
     console.log(filteredData);
+
+    var tableBody = d3.select("tbody");
+
+    tableBody.html("");
+
+    filteredData.forEach((alien) => {
+        var row = tableBody.append("tr");
+        Object.entries(alien).forEach(([key,value]) =>{
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
 }
